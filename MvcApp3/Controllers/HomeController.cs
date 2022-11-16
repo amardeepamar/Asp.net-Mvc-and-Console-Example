a6253d7c844bc3ccce96d7738eabbb32afa5e7b2
Amardeep Amar <amardeepamar.aa@outlook.com>
2022-11-16 16:50:17 +0530

    Razor if Condition

---
+++ MvcApp3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Home/Expression
+        public ActionResult Expression()
+        {
+            return View();
+        }
+        // GET: Home/RazorIfExample
+        public ActionResult RazorIfExample()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ MvcApp3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApp3.MvcApplication" Language="C#" %>
